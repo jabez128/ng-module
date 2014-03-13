de5023a1f3414d03c14d9af8b45a0b6a3fc4d2ec
jabez zhang <jabez128@163.com>
2014-03-13 09:44:57 +0800

    添加了job部分的内容
    添加了job部分的内容

--- app/app.js
+++ app/app.js
@@ -8,17 +8,30 @@ var app = angular.module('myapp',['ngRoute'])
                       templateUrl:'./view/detail.html',
                       controller:'module'
                    })
+                   .when('/job',{
+                     templateUrl: './view/job.html',
+                     controller: 'job'
+                   })
                    .otherwise({
                      redirectTo:'/'
                    });
                 $locationProvider.hashPrefix('!');
                  });
 
-   app.controller('main',function($scope){
-
+   app.controller('main',function($scope,$location){
+     $scope.gojob = function(){
+        $location.path('/job');
+     };
 
    });
 
    app.controller('module',function($scope,$routeParams){
        $scope.name = $routeParams.module_name;
      });
+
+   app.controller('job',function($scope,$location){
+     $scope.gomain = function(){
+        $location.path('/');
+     };
+     $scope.visitors = 0;
+   });
