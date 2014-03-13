c05393bbb0f2b79e8708edc32b53f6cb4846efef
jabez zhang <jabez128@163.com>
2014-03-13 16:22:12 +0800

    添加了ng-book部分，解决了逆序排序的问题
    添加了ng-book部分，解决了逆序排序的问题

--- app/app.js
+++ app/app.js
@@ -14,6 +14,9 @@ var app = angular.module('myapp',['ngRoute','firebase','btford.markdown'])
                    }).when('/edit',{
                      templateUrl: './view/edit.html',
                      controller: 'edit'
+                   }).when('/book',{
+                     templateUrl: './view/book.html',
+                     controller: 'book'
                    })
                    .otherwise({
                      redirectTo:'/'
@@ -24,17 +27,39 @@ var app = angular.module('myapp',['ngRoute','firebase','btford.markdown'])
     /*
     **下面是编辑指令的部分
     */
-
+    app.filter('reverse', function() {
+      function toArray(list) {
+         var k, out = [];
+         if( list ) {
+            if( angular.isArray(list) ) {
+               out = list;
+            }
+            else if( typeof(list) === 'object' ) {
+               for (k in list) {
+                  if (list.hasOwnProperty(k)) { out.push(list[k]); }
+               }
+            }
+         }
+         return out;
+      }
+      return function(items) {
+         return toArray(items).slice().reverse();
+      };
+   });
 
 
   /*
   **下面是控制器部分
   */
    app.controller('main',function($scope,$firebase){
+     $scope.loaded = false;
      var ref1 = new Firebase('https://ng-module.firebaseIO.com');
      var ref2 = new Firebase('https://ng-visitor.firebaseIO.com');
      $scope.modules = $firebase(ref1);
      $scope.visitor = $firebase(ref2);
+     $scope.modules.$on('change',function(){
+       $scope.loaded = true;
+     });
      var interval = setInterval(function(){
        console.log($scope.visitor.number);
        if(typeof $scope.visitor.number === 'number'){
@@ -51,10 +76,14 @@ var app = angular.module('myapp',['ngRoute','firebase','btford.markdown'])
      });
 
    app.controller('job',function($scope,$firebase){
+    $scope.loaded = false;
     var ref1 = new Firebase('https://ng-visitor.firebaseIO.com');
     $scope.visitor = $firebase(ref1);
     var ref2 = new Firebase('https://ng-job.firebaseIO.com');
     $scope.jobs = $firebase(ref2);
+    $scope.jobs.$on('change',function(){
+      $scope.loaded = true;
+    })
    });
 
    app.controller('edit',function($scope,$firebase,$window,$location){
@@ -63,8 +92,12 @@ var app = angular.module('myapp',['ngRoute','firebase','btford.markdown'])
      $scope.jobs = $firebase(ref);
      $scope.submit = function(){
        $scope.jobs.$add({title: $scope.post.title, content: $scope.post.content}).then(function(ref) {
-           $window.alert('添加成功！');
+           $window.alert(ref.name());
            $location.path('/job');
     });
      }
+   });
+
+   app.controller('job',function($scope,$firebase){
+     
    })
